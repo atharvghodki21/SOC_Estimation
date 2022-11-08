f27861b02680b66f8accbc459b5839eb66dd2009
atharvghodki21 <atharvghodki21@gmail.com>
2022-11-08 20:08:26 +0530

    Update SOC_CalculatorPanel.java

--- src/SOC_CalculatorPanel.java
+++ src/SOC_CalculatorPanel.java
@@ -21,37 +21,44 @@ public class SOC_CalculatorPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public SOC_CalculatorPanel() {
-		setBounds(0, 0, 592, 493);
+//		setBounds(0, 0, 592, 493);
+		setBounds(0, 0, 1100, 650);
 		setLayout(null);
 		
 		JLabel MainHeading = new JLabel("SOC Calculator Using Coulomb Counting Method");
-		MainHeading.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		MainHeading.setBounds(10, 27, 390, 29);
+		MainHeading.setFont(new Font("Tahoma", Font.BOLD, 30));
+		MainHeading.setBounds(194, 32, 765, 35);
 		add(MainHeading);
 		
 		JLabel lblNewLabel = new JLabel("Enter initial SOC :");
-		lblNewLabel.setBounds(10, 117, 163, 14);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblNewLabel.setBounds(268, 117, 361, 27);
 		add(lblNewLabel);
 		
 		JLabel lblSoh = new JLabel("SOH :");
-		lblSoh.setBounds(10, 155, 89, 14);
+		lblSoh.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblSoh.setBounds(268, 155, 361, 27);
 		add(lblSoh);
 		
 		JLabel lblDod = new JLabel("DOD : ");
-		lblDod.setBounds(10, 192, 89, 14);
+		lblDod.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblDod.setBounds(268, 192, 361, 27);
 		add(lblDod);
 		
 		JLabel lblEnterBatteryCurrent = new JLabel("Enter battery current Ib (mA) : ");
-		lblEnterBatteryCurrent.setBounds(10, 231, 179, 14);
+		lblEnterBatteryCurrent.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblEnterBatteryCurrent.setBounds(268, 231, 361, 27);
 		add(lblEnterBatteryCurrent);
 		
 		JLabel lblEnterBatteryVoltage = new JLabel("Enter battery voltage Vb (Volts) : ");
-		lblEnterBatteryVoltage.setBounds(10, 274, 203, 14);
+		lblEnterBatteryVoltage.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblEnterBatteryVoltage.setBounds(268, 274, 361, 27);
 		add(lblEnterBatteryVoltage);
 		
 //		-------------------------------------------------------------------------------------------------------------------
 		
 		JButton jButtonSubmit = new JButton("Submit Parameters");
+		jButtonSubmit.setFont(new Font("Tahoma", Font.PLAIN, 21));
 		jButtonSubmit.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -165,13 +172,14 @@ public class SOC_CalculatorPanel extends JPanel {
 			}  // MouseClicked Function Ends
 		}); // Add mouseListener ends
 		
-		jButtonSubmit.setBounds(0, 312, 173, 23);
+		jButtonSubmit.setBounds(398, 349, 319, 27);
 		add(jButtonSubmit);
 		
 		
 //		--------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
 		JButton jButtonReset = new JButton("Reset Parameters");
+		jButtonReset.setFont(new Font("Tahoma", Font.PLAIN, 21));
 		jButtonReset.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -185,37 +193,52 @@ public class SOC_CalculatorPanel extends JPanel {
                 }
 			} // MouseClicked Function Ends
 		});  // Add mouseListener ends
-		jButtonReset.setBounds(0, 344, 173, 23);
+		jButtonReset.setBounds(398, 381, 319, 27);
 		add(jButtonReset);
 		
 		
 //		------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
 		jTextFieldInitial_SOC = new JTextField();
-		jTextFieldInitial_SOC.setBounds(253, 114, 86, 20);
+		jTextFieldInitial_SOC.setBounds(747, 117, 86, 20);
 		add(jTextFieldInitial_SOC);
 		jTextFieldInitial_SOC.setColumns(10);
 		
 		jTextField_SOH = new JTextField();
 		jTextField_SOH.setText("100");
 		jTextField_SOH.setColumns(10);
-		jTextField_SOH.setBounds(253, 152, 86, 20);
+		jTextField_SOH.setBounds(747, 155, 86, 20);
 		jTextField_SOH.setEditable(false);
 		add(jTextField_SOH);
 		
 		jTextFieldInitial_DOD = new JTextField();
 		jTextFieldInitial_DOD.setColumns(10);
-		jTextFieldInitial_DOD.setBounds(253, 189, 86, 20);
+		jTextFieldInitial_DOD.setBounds(747, 192, 86, 20);
 		add(jTextFieldInitial_DOD);
 		
 		jTextField_Ib = new JTextField();
 		jTextField_Ib.setColumns(10);
-		jTextField_Ib.setBounds(253, 228, 86, 20);
+		jTextField_Ib.setBounds(747, 231, 86, 20);
 		add(jTextField_Ib);
 		
 		jTextField_Vb = new JTextField();
 		jTextField_Vb.setColumns(10);
-		jTextField_Vb.setBounds(253, 271, 86, 20);
+		jTextField_Vb.setBounds(747, 274, 86, 20);
 		add(jTextField_Vb);
+		
+		JLabel lblNewLabel_1 = new JLabel("This calculator calculates the State of Charge (SOC) and State of Health (SOH) of Lithium-ion batteries.");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(194, 473, 765, 35);
+		add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("batteries. It gives indication about the battery usage and also indicates about the charging and");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(194, 519, 765, 35);
+		add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("discharging mode of the battery to be tested.");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_3.setBounds(194, 565, 765, 35);
+		add(lblNewLabel_3);
 	}
 }
